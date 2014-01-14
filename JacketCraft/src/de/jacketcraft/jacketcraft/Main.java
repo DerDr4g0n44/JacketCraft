27b2326ce039212934ab68ee017889977031218e
SuperChipsLP <nikilp26@gmail.com>
2014-01-14 20:35:28 +0100

    Created main files.

---
+++ JacketCraft/src/de/jacketcraft/jacketcraft/Main.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.jacketcraft.jacketcraft;
+
+/**
+ *
+ * @author Niklas_2
+ */
+public class Main {
+    
+}
